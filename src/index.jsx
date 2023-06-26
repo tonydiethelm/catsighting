3ff2200e2f80974b402a926b3de09be922d8991b
Tony Diethelm <tonydiethelm@gmail.com>
2023-06-25 18:48:44 -0700

    further progress on JSX

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,15 @@
 console.log('test to make sure the JSX is loaded');
 
 import {useState} from "react";
+
+
+function catElement(catName, lastSighting){
+  return(
+    <div className="catElement">
+      <img src=`{catName}.jpg alt="Picture of a cat"></img>
+      <p>{catName} was last seen: {lastSighting}</p>  
+
+
+    </div>
+  );
+}
