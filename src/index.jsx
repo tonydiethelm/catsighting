8ea088758f843b90d85dac946e302e0255a7ad81
Tony Diethelm <tonydiethelm@gmail.com>
2023-06-28 17:07:39 -0700

    Final decision made on FE state and flow. Design doc updated.

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,21 @@
 console.log('test to make sure the JSX is loaded');
 
 import {useState} from "react";
+const cats = ["Pan", "LokiSlugBear"];
 
 
 function catElement(catName, lastSighting){
   return(
     <div className="catElement">
-      <img src=`{catName}.jpg alt="Picture of a cat"></img>
-      <p>{catName} was last seen: {lastSighting}</p>  
-
+      <img src={catName} alt="Picture of a cat"></img>
+      <p>{catName} was last seen: {lastSighting}</p>
+      <button type="button" className="catButton" onClick={updateSighting}>I saw {catName} today.</button>
 
     </div>
   );
 }
+
+
+
+
+
