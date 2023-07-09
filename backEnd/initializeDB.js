c87e850c0814a26d8343eb7515a2f76ca834be89
Tony Diethelm <tonydiethelm@gmail.com>
2023-07-08 20:46:30 -0700

    Attempting to connec to my redis server

--- backEnd/initializeDB.js
+++ backEnd/initializeDB.js
@@ -5,6 +5,7 @@ Set up the DB with proper cat data
 
 As Redis is key:value, i need a key (cats) and my value (my array, as per above). But I'll need to JSONify my array. 
 
+https://redis.io/docs/clients/nodejs/
 
 */
 
@@ -12,18 +13,27 @@ As Redis is key:value, i need a key (cats) and my value (my array, as per above)
 //setup stuff
 import {todaysDate} from './todaysDate.mjs';
 import redis from 'redis';
-
+const port = 6379;
+const server = "192.168.0.3";
+const connectionString = 'redis://services.shc:6380'
 
 //main
 //connect to DB
+const client = redis.createClient({url: connectionString})
+client.connect()
+.then(() => console.log('connected!'))
+.catch((error) => console.log('We had an error: ', error))
+
 //JSONify the data
-//write to DB
-
-const client = redis.createClient({url: 'redis://service'})
-client.on('error', err => console.log('Redis Server Error', err));
-
 const cats = [{name: 'SlugBear', lastSighting: 'today'}, {name: 'Pan', lastSighting: 'today'}]
 const JSONcats = JSON.stringify(cats);
-console.log('I am going to send...', JSONcats);
+console.log('I am going to send...', JSONcats, typeof(JSONcats));
 
-console.log(await client.set('cats', JSONcats)); // 'OK'
+//write to DB
+
+
+
+
+
+
+//console.log(await client.set('cats', 'test')); // 'OK'
